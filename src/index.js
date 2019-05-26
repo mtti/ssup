7f524e51d9c6449e2df33ef34660648974f4b9cd
mtti <mtti@users.noreply.github.com>
2019-05-26 21:13:48 +0300

    Only export the Uploader class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-const uploader = require('./uploader');
-const utils = require('./utils');
-const cloudfront = require('./cloudfront');
-const localfs = require('./local-fs');
+const { Uploader } = require('./uploader');
 
 module.exports = {
-  ...uploader,
-  ...utils,
-  ...cloudfront,
-  ...localfs,
+  Uploader,
 };
